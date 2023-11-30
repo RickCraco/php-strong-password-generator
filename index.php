b280b6b9cd1497010c829ae0151bc7d1dd7ab506
RickCraco <riccardocracolici43@gmail.com>
2023-11-30 16:57:30 +0100

    aggiunto stile al form

--- index.php
+++ index.php
@@ -3,38 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
 
-    <h1>Password Generator</h1>
+    <h1 class="text-center">Password Generator</h1>
+    <div class="container">
+        <form action="functions.php" method="post">
 
-    <form action="functions.php" method="post">
+            <label for="length">Password length: </label>
+            <input type="number" name="length" min="6" max="20" class="form-control w-25">
 
-        <label for="length">Password length: </label>
-        <input type="number" name="length" min="6" max="20">
+            <br>
 
-        <br>
+            <label for="numbers">Use numbers:</label>
+            <input type="checkbox" name="numbers" class="form-check-input">
+            <br>
 
-        <label for="numbers">Use numbers:</label>
-        <input type="checkbox" name="numbers">
-        <br>
+            <label for="letters">Use letters:</label>
+            <input type="checkbox" name="letters" class="form-check-input">
+            <br>
 
-        <label for="letters">Use letters:</label>
-        <input type="checkbox" name="letters">
-        <br>
+            <label for="symbols">Use symbols:</label>
+            <input type="checkbox" name="symbols" class="form-check-input">
+            <br>
 
-        <label for="symbols">Use symbols:</label>
-        <input type="checkbox" name="symbols">
-        <br>
+            <label for="repete">Repete characters:</label>
+            <input type="checkbox" name="repete" class="form-check-input">
+            <br>
 
-        <label for="repete">Repete characters:</label>
-        <input type="checkbox" name="repete">
-        <br>
-
-        <button type="submit">Generate</button>
-        <button type="reset">Reset</button>
-    </form>
+            <button type="submit" class="btn btn-primary">Generate</button>
+            <button type="reset" class="btn btn-danger">Reset</button>
+        </form>
+    </div>
+   
 
 </body>
 </html>
