bcf956232c1a2e8d92b43f637e3201e1ff7780cb
RickCraco <riccardocracolici43@gmail.com>
2023-12-02 14:02:57 +0100

    refacttoring completo della funzione, ora non piú controllo sulla singola checkbox , ma su array contentente le checkbox

--- functions.php
+++ functions.php
@@ -1,59 +1,64 @@
-    <?php
-    session_start();
+<?php 
 
-    function generatePassword($length, $use_numbers, $use_letters, $use_symbols, $allow_repetition) {
-        
-        $characters = '';
-        if ($use_numbers) $characters .= '0123456789';
-        if ($use_letters) $characters .= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        if ($use_symbols) $characters .= '!@#$%^&*()-_+=<>?';
+if(isset($_POST['length'])){
+    $length = $_POST['length'];
 
-        $password = '';
+    if(!empty($_POST['repete'])){
+        $repete = $_POST['repete'];
+    }else{
+        $repete = "0";
+    }
+    if(!empty($_POST['characters'])){
+        $characters = $_POST['characters'];
+    }else{
+        $characters = ["letters"];
+    }
 
-        if($allow_repetition == true){
-            for ($i = 0; $i < $length; $i++) {
-                $index = rand(0, strlen($characters) - 1);
-                $password .= $characters[$index];
-                //var_dump($password);
-            }
-        }else{
-            while(strlen($password) < $length){
-                $newCharacter = $characters[rand(0, strlen($characters) - 1)];
-                if(!strpos($password, $newCharacter)){
-                    $password .= $newCharacter;
-                }
+    $password = generaPassword($length, $repete, $characters);
+
+    echo '<h3>'.$password.'</h3>';
+}
+
+function generaPassword($length, $repete, $characters){
+    $letters = 'abcdefghijklmnopqrstuvwxyz';
+    $numbers = '0123456789';
+    $symbols = '!@#$%&';
+
+    $stringtoUse = '';
+
+    $password = '';
+
+    // se l'array é vuoto voglio che stringtoUse sia concatenata a tutti i numeri lettere e simboli
+
+    if(count($characters) > 0){
+
+        if(in_array('letters', $characters)){
+            $stringtoUse .= $letters;
+        }
+        if(in_array('numbers', $characters)){
+            $stringtoUse .= $numbers;
+        }
+        if(in_array('symbols', $characters)){
+            $stringtoUse .= $symbols;
+        }
+    }else{
+        $stringtoUse = $letters.$numbers.$symbols;
+    }
+
+    if($repete === "1"){
+        for($i = 0; $i < $length; $i++){
+            $password .= $stringtoUse[rand(0, strlen($stringtoUse) - 1)];
+        }
+    }elseif($repete === "0"){
+        while(strlen($password) < $length){
+            $newCharacter = $stringtoUse[rand(0, strlen($stringtoUse) - 1)];
+            if(!strpos($password, $newCharacter)){
+                $password .= $newCharacter;
             }
         }
-
-        return $_SESSION['password'] = $password;
     }
 
-    //var_dump($_SESSION['prova']);
+    return $password;
+}
 
-    if(isset($_POST['length'])){
-        $length = $_POST['length'];
-        $numbers = isset($_POST['numbers']);
-        $letters = isset($_POST['letters']);
-        $symbols = isset($_POST['symbols']);
-        $repete = isset($_POST['repete']);
-
-        $password=generatePassword($length, $numbers, $letters, $symbols, $repete);
-        //$_SESSION['pippo'] = $password;
-        //var_dump($_SESSION['pippo']);
-        //header('Location: showpassword.php');
-        //exit();
-    }
-        
-        echo '<h2>Your Password:</h2>';
-        echo '<h3>' . $password . '</h3>';
-        
-        /*
-        if($password){
-            $_SESSION['password'] = $password;
-            header('Location: showpassword.php');
-            exit();
-        }else{
-            echo '<h2>Your Password:</h2>';
-            echo '<p>' . $password . '</p>';
-        }  */
-    ?>
+?>
