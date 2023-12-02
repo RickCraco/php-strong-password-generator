f0c18708eb46b9a184e46a353bb06db40774f0f5
RickCraco <riccardocracolici43@gmail.com>
2023-12-02 14:01:28 +0100

    refactoring completo del codice

--- index.php
+++ index.php
@@ -1,51 +1,36 @@
-<?php 
-session_start();
-
-$_SESSION['prova'] = 'ciao';
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
+    <h1>Generatore di password</h1>
 
-    <h1 class="text-center">Password Generator</h1>
-    <div class="container">
-        <form action="functions.php" method="POST">
+    <form action="functions.php" method="POST">
+        <label for="length">Inserisci la lunghezza della password</label>
+        <input type="number" name="length">
 
-            <label for="length">Password length: </label>
-            <input type="number" name="length" min="6" max="20" class="form-control w-25">
+        <br>
 
-            <br>
+        <label for="repetition">Consenti ripetizioni</label>
+        <input type="radio" name="repete" value="1">
+        <label for="repetition">Non consenti ripetizioni</label>
+        <input type="radio" name="repete" value="0">
 
-            <label for="numbers">Use numbers:</label>
-            <input type="checkbox" name="numbers" value="numbers" class="form-check-input">
-            <br>
+        <br>
 
-            <label for="letters">Use letters:</label>
-            <input type="checkbox" name="letters" value="letters" class="form-check-input">
-            <br>
+        <label for="letters">Lettere</label>
+        <input type="checkbox" name="characters[]" value="letters">
+        <br>
+        <label for="numbers">Numeri</label>
+        <input type="checkbox" name="characters[]" value="numbers">
+        <br>
+        <label for="symbols">Simboli</label>
+        <input type="checkbox" name="characters[]" value="symbols">
 
-            <label for="symbols">Use symbols:</label>
-            <input type="checkbox" name="symbols" value="symbols" class="form-check-input">
-            <br>
-
-            <label for="repete">Repete characters:</label>
-            <input type="checkbox" name="repete" class="form-check-input">
-            <br>
-
-            <button type="submit" class="btn btn-primary">Generate</button>
-            <button type="reset" class="btn btn-danger">Reset</button>
-        </form>
-    </div>
-   
-
+        <button type="submit">Genera</button>
+    </form>
 </body>
 </html>
